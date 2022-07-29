89669116653562e6186d2046f9f9b101ac483d74
brenesamerica <56234623+brenesamerica@users.noreply.github.com>
2022-07-29 22:29:32 +0200

    Update providers.tf

--- examples/eks-getting-started/providers.tf
+++ examples/eks-getting-started/providers.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.21"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 3.4"
